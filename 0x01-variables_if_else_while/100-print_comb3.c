64cdd8e391d9fea670ef91698e2ac88602a00dc4
Odu, Odu E <oduoduemmanuel@yahoo.com>
2022-09-09 14:33:13 -0700

    for loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Print all combinations of two digit numbers
+ *
+ * Result: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	int nums;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (nums = num + 1; nums <= 9; nums++)
+		{
+			putchar(num + '0');
+			putchar(nums + '0');
+
+			if (num < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
