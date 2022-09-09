4467d190892a44f20eb1ca7e2ae345e0fc0a742b
Odu, Odu E <oduoduemmanuel@yahoo.com>
2022-09-08 18:18:38 -0700

    hello c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - prints and that piece of art is useful" - Dora Korpor, 2015-10-19
  *
- * Return: Always 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",61);
-
+write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
