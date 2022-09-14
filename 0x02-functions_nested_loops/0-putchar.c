1c4005dedeb0dc647aba004f4d53159478d92ff6
Odu, Odu E <oduoduemmanuel@yahoo.com>
2022-09-13 18:39:33 -0700

    go

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,49 +1,13 @@
 #include "main.h"
 
-<<<<<<< HEAD
-
-
-/**
-
- * main -  prints _putchar, followed by a new line
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
-{
-
-	_putchar('_');
-
-	_putchar('p');
-
-	_putchar('u');
-
-	_putchar('t');
-
-	_putchar('c');
-
-	_putchar('h');
-
-	_putchar('a');
-
-	_putchar('r');
-
-	_putchar('\n');
-
-
-
-	return (0);
-
-=======
 /**
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -55,5 +19,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
->>>>>>> 92e2ceea073d640b2c2750355cf22300a41d7881
 }
