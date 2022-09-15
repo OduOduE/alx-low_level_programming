63725047b006ed8983072a7664a6c2f8bffc243d
Odu, Odu <oduoduemmanuel@yahoo.com>
2022-09-15 21:43:23 +0000

    More nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - a function to print a straight line
+ * Return - Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+	char c;
+
+	for (c = 0; c < n; c++)
+	_pitchar('_');
+	_putchar('\n');
+}
